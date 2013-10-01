0b44a18c46d166fc472912eadd7dbec66874be28
Pat <ssabpisa@chives.ecn.purdue.edu>
2013-10-01 16:42:40 -0400

    Loop reading data

--- dev_vip/setup.c
+++ dev_vip/setup.c
@@ -30,18 +30,21 @@ dispatchProcess: manages spawning of filter based on filterskey
 void command_listen(Settings * st){
    struct sockaddr_rc localaddr = { 0 }; 
    struct sockaddr_rc remoteaddr = { 0 };
-   char buf[1024] = { 0 };
+   char buf[2048] = { 0 };
    int s, client, bytes_read;
    socklen_t opt = sizeof(remoteaddr); //Size of Address
    //connect to socket
+   printf("Connecting to socket..\n");
    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
    localaddr.rc_family = AF_BLUETOOTH;
    localaddr.rc_bdaddr = *BDADDR_ANY;
    localaddr.rc_channel = (uint8_t) 1; //port 1
    //bind socket to port 1 of first available BT adapter
+   printf("Binding socket to port..\n");
    bind(s, (struct sockaddr *)&localaddr, sizeof(localaddr));
    listen(s, 1); //backlog = 1, maximum length to which the queue of pending connection may grow
    //begin accepting connections
+   printf("Waiting for client..\n");
    client = accept(s, (struct sockaddr *)&remoteaddr, &opt); 
    //convert address structure to string
    ba2str(&remoteaddr.rc_bdaddr, buf);
@@ -50,7 +53,8 @@ void command_listen(Settings * st){
    memset(buf, 0, sizeof(buf));
  
    //keep reading until halt is encountered
-   while(strcmp(buf,"halt")){
+   //while(strcmp(buf,"halt") != 0){
+   while(1){
       bytes_read = read(client, buf, sizeof(buf));
       if(bytes_read > 0){
          printf("received: %s\n", buf);
@@ -58,6 +62,7 @@ void command_listen(Settings * st){
       /* TRY:
          recv(s, buf, sizeof(buf), 0);
       */
+      memset(buf,0,sizeof(buf)); 
    }
    
    close(client);
