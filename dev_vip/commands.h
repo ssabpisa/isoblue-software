5f070784b07893fda2879ec28b1c882cb8d20d0b
Pat <ssabpisa@chives.ecn.purdue.edu>
2013-10-02 16:19:15 -0400

    Added more commands

--- dev_vip/commands.h
+++ dev_vip/commands.h
@@ -1,4 +1,9 @@
 #define SIGNAL_FINISH "isoend"
+#define SIGNAL_SETFILTER "setfilter"
+#define SIGNAL_ENDLIST "endlist"
+#define SIGNAL_SETFILTERMODE "setfiltermode"
+#define SIGNAL_FILTER_BLOCK "block"
+#define SIGNAL_FILTER_ALLOW "allow"
 
 void scan(char * addr);
 Settings * command_listen(Settings * st);
