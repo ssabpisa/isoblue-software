25edb09ad031890136c1dcd07260f3cece20ec7f
ssabpisa <ssabpisa@me.com>
2013-10-02 23:09:30 -0400

    bt still hang sometimes, added setting file output

--- dev_vip/setup.c
+++ dev_vip/setup.c
@@ -3,8 +3,8 @@
 * Description: runs in background and waits for command sent via bluetooth by the BBB.
 * 
 * Author : Pat SSabpisal  <ssabpisa@purdue.edu>
-*        : 
-*        :
+*        : <contributors> 
+*        : <contributors>
 */
 
 
@@ -36,18 +36,19 @@ Settings * command_listen(Settings * st){
    //connect to socket
    printf("Connecting to socket..\n");
    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+   //defining the family of radio to use
    localaddr.rc_family = AF_BLUETOOTH;
    localaddr.rc_bdaddr = *BDADDR_ANY;
    localaddr.rc_channel = (uint8_t) 1; //port 1
    //bind socket to port 1 of first available BT adapter
    printf("Binding socket to port..\n");
    bind(s, (struct sockaddr *)&localaddr, sizeof(localaddr));
-   listen(s, 1); //backlog = 1, maximum length to which the queue of pending connection may grow
+   listen(s, 5); //backlog = 1, maximum length to which the queue of pending connection may grow
    //begin accepting connections
    printf("Waiting for client..\n");
-   while(1){
+   while(1){ //keep trying 
    	client = accept(s, (struct sockaddr *)&remoteaddr, &opt); 
-        if(client > 0) break;
+        if(client >= 0) break;
    }
    //convert address structure to string
    ba2str(&remoteaddr.rc_bdaddr, buf);
@@ -57,14 +58,18 @@ Settings * command_listen(Settings * st){
    
 
    Settings * sob = calloc(sizeof(Settings),1);
-   int countcmd = 0;
-   while(1){
+   int countcmd = 0; //count how many command has been issued from client
+   int countpgn = 0; //count how many pgn has been set (temp)
+   int MODE_SETFILTER = 0; //flag to check what type of data is the client sending
+   sob->PGN_sets = malloc(sizeof(int)*20); /*max 20 pgns , for now*/
+
+   while(1){ //keep receiving until stop is encountered
       bytes_read = read(client, buf, sizeof(buf));
       if(bytes_read > 0){
          printf(">> %s\n", buf);
 	 countcmd += 1;
       }
-      /* TRY:
+      /* TODO:try
          recv(s, buf, sizeof(buf), 0);
       */
       
@@ -72,16 +77,33 @@ Settings * command_listen(Settings * st){
         //if buffer is "finish"
 	printf("FINISH SIGNAL received!!\n");
 	printf("Spawning Filter..\n");
-	system("./filter vcan0");
+	
+	system("./filter vcan0 vcan0");
+        //Note: above commands should pass in "setting file" 
+	// filter.c need to open up setting file (binary) and read the PGN_sets data
+
 	//TODO: replace system with dispatch process
+
         printf("\nReturning to Wait State..\n");
   	break;
       }
      
-      if (!strcmp(buf,SIGNAL_SETFILTER)){
-	printf("Recognized Request to set filter\n");
+      if (!strcmp(buf,SIGNAL_SETFILTER)){ //if command is to set filter (defined in commands.h)
+ 	printf("Recognized Request to set filter\n");
+	MODE_SETFILTER = 1;  //mark the flag 
       }
-      memset(buf,0,sizeof(buf)); 
+      
+      if(MODE_SETFILTER){ //if flagged
+        printf("Adding %s to filter (%d)\n", buf, countpgn);
+	sob->PGN_sets[countpgn++] = atoi(buf); //add to pgn sets
+      }
+
+      if(!strcmp(buf,SIGNAL_ENDLIST)){ //if command is to finish adding to list
+	MODE_SETFILTER = 0; //unflag the register
+	printf("Filter saved\n"); 
+      }
+
+      memset(buf,0,sizeof(buf)); //clear buffer so we can start reading again
    }
    
 
