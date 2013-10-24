44832ea1527031e88f15622959c7fdb51170c331
Joseph Chiu <chiu12@chives.ecn.purdue.edu>
2013-10-23 20:05:44 -0400

    This is adding the #include statements

--- tools/compression.c
+++ tools/compression.c
@@ -1,3 +1,12 @@
 /*
     Library for Huffman Compression
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <compression.h>
+
+#define len(x) ((int)log(x)+1)
+
+/*Node of the huffman tree */
+struct node
