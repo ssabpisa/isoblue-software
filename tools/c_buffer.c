f9dc6fe21e517c7515742f96f68042062457a22a
DMA-013 <apple@pal-nat184-194-227.itap.purdue.edu>
2013-11-11 13:28:19 -0500

    Changes

--- tools/c_buffer.c
+++ tools/c_buffer.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 
-void copy_to_permanent_storage(struct isobus_message mes, struct timeval tv, int iface, uint8_t addr, FILE *fptr1); //the function to store the messages permanently
+void copy_to_permanent_storage(struct isobus_mesg mes, struct timeval tv, int iface, uint8_t addr, FILE *fptr1); //the function to store the messages permanently
 
-void copy_to_permanent_storage(struct isobus_message mes, struct timeval tv, int iface, uint8_t addr, FILE *fptr1)
+void copy_to_permanent_storage(struct isobus_mesg mes, struct timeval tv, int iface, uint8_t addr, FILE *fptr1)
 {
 	
 	fprintf(fptr1, "%d %d %d %d %d",iface, mes.pgn, tv.tv_sec, tv.tv_usec, addr);
