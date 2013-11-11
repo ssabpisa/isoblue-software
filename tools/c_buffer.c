45dd25a4c677ce01c197dbccb0f5f1684e64148a
DMA-013 <apple@pal-nat184-194-227.itap.purdue.edu>
2013-11-11 13:25:37 -0500

    sdkfl;

--- tools/c_buffer.c
+++ tools/c_buffer.c
@@ -8,7 +8,7 @@ void copy_to_permanent_storage(struct isobus_message mes, struct timeval tv, int
 void copy_to_permanent_storage(struct isobus_message mes, struct timeval tv, int iface, uint8_t addr, FILE *fptr1)
 {
 	
-	fprintf(fptr1, "%d %d %d %d %d ",iface, mes.pgn, tv.tv_sec, tv.tv_usec, addr);
-
+	fprintf(fptr1, "%d %d %d %d %d",iface, mes.pgn, tv.tv_sec, tv.tv_usec, addr);
+    fflush (fptr1);
 
 }
