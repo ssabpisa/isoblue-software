af2ebd12163a4207d62137940a5d30510fab60ca
DMA-013 <apple@pal-nat184-206-57.itap.purdue.edu>
2013-11-15 12:40:01 -0500

    Adjusting cbuffer.c

--- tools/c_buffer.c
+++ tools/c_buffer.c
@@ -11,11 +11,11 @@ void copy_to_permanent_storage(struct isobus_mesg mes, struct timeval tv, int if
 {
 	int i;
 
-	fprintf(fptr1, " Socket: %d PGN: %d ", iface, mes.pgn);
+	fprintf(fptr1, " Socket: %d PGN: %d Data: ", iface, mes.pgn);
 
     for (i = 0; i < mes.dlen; i++)
     {
-       fprintf(fptr1, "%d", mes.data[i]);//Array of the data
+       fprintf(fptr1, " %02x ", mes.data[i]);//Array of the data
     
     }
 
