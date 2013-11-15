3d1c21f1ebbe11d0b2e12ef30aa773ac98694b6f
DMA-013 <apple@pal-nat184-206-57.itap.purdue.edu>
2013-11-15 12:09:54 -0500

    Added something

--- tools/c_buffer.c
+++ tools/c_buffer.c
@@ -5,10 +5,13 @@
 
 void copy_to_permanent_storage(struct isobus_mesg mes, struct timeval tv, int iface, uint8_t addr, FILE *fptr1); //the function to store the messages permanently
 
+
+
 void copy_to_permanent_storage(struct isobus_mesg mes, struct timeval tv, int iface, uint8_t addr, FILE *fptr1)
 {
 	
-    fprintf(fptr1, "messages: %d %d %d %d %d\n",iface, mes.pgn, tv.tv_sec, tv.tv_usec, addr);
+    fprintf(fptr1, "messages and socket: %d %d Time: %d:%d Address:%d \n",iface, mes.pgn, tv.tv_sec, tv.tv_usec, addr);
     fflush (fptr1);
 
 }
+
