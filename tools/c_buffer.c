a2d8686568587832784d130d97749813710f59c6
DMA-013 <apple@cary-f-116.resnet.purdue.edu>
2013-11-30 03:53:49 -0500

    Added the search for last line of the txt file

--- tools/c_buffer.c
+++ tools/c_buffer.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+FILE *sync;
+
+
 
 void copy_to_permanent_storage(struct isobus_mesg mes, struct timeval tv, int iface, uint8_t addr, FILE *fptr1); //the function to store the messages permanently
-//void check();
-
+void check(/*Have the android timestamp*/); //may need to be change
 
 void copy_to_permanent_storage(struct isobus_mesg mes, struct timeval tv, int iface, uint8_t addr, FILE *fptr1)
 {
@@ -22,16 +24,63 @@ void copy_to_permanent_storage(struct isobus_mesg mes, struct timeval tv, int if
     fprintf(fptr1, " Time: %d.%06d Address: %d \n", tv.tv_sec, tv.tv_usec, addr);
     fflush (fptr1);
 
+
 }
 
-// void check(/*TimeStamp*/)
-// {
-// 	char us[6]; //to hold the string for the mircoseconds
-// 	char ns[16]; //to hold the timestamp (seconds) from database.txt
-	
-// 	fp = fopen("database.txt",r);
-// 	fscanf(fptr1,"%d.%0.6d",ns,us);
-	
 
-// 	if (/*TimeStamp*/ == )
-// }
+//Issue, how to run this function once the first function stops
+void check(/*TimeStamp from android*/)
+{
+    char line[1024] = {0,};
+    int n2 = 0;
+    int n3 = 0;
+    
+    double r_1 = 0;
+    double r_2 = 0;
+    double result = 0;
+
+    double c_num = 1385144575.788001; 
+
+    int hold;
+
+
+    sync = fopen("database.txt","r");
+    
+    
+    if(sync != NULL) 
+    {
+        while( fgets(line, 1024, sync) != NULL)
+        {
+            //Nothing
+        }
+
+        fclose(sync);
+
+        if (sscanf(line, "%*[^T]Time: %d.%d", &n2, &n3))
+        {
+            //to convert the n2 and n3 into the timestamp
+            r_1 = n2;
+            r_2 = n3 / 1000000.0;
+            result = r_1 + r_2;
+            // printf("%.6f\n", result);
+            // printf("%.6f\n", c_num);
+            if ( result == c_num)
+            {
+                printf("Success\n");
+                hold = 0;
+            }
+
+            else
+            {
+                printf("No Success\n");
+                hold = 1;
+            }
+
+            //The result from txt file
+            //printf("%d.%d\n", n2, n3);
+        }
+    }
+
+   
+   
+}
